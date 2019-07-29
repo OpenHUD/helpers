1ac77fcedcaff56d93ba470065ddffb189eff385
Danny Leshem <dleshem@gmail.com>
2019-07-29 13:33:35 +0300

    Refactoring

--- test/represent.test.js
+++ test/represent.test.js
@@ -2,23 +2,25 @@ import represent from '../src/represent.js';
 import {expect} from 'chai';
 
 describe('represent', () => {
-    it ('supports pocket pairs', () => {
-        const hand = ['As', 'Ad'];
-        expect(represent({hand})).to.equal('AA');
-    });
+    describe('2 cards', () => {
+        it ('supports pocket pairs', () => {
+            const hand = ['As', 'Ad'];
+            expect(represent({hand})).to.equal('AA');
+        });
 
-    it ('supports suited hands', () => {
-        const hand = ['As', 'Ks'];
-        expect(represent({hand})).to.equal('AKs');
-    });
+        it ('supports suited hands', () => {
+            const hand = ['As', 'Ks'];
+            expect(represent({hand})).to.equal('AKs');
+        });
 
-    it ('supports offsuit hands', () => {
-        const hand = ['As', 'Kd'];
-        expect(represent({hand})).to.equal('AKo');
-    });
+        it ('supports offsuit hands', () => {
+            const hand = ['As', 'Kd'];
+            expect(represent({hand})).to.equal('AKo');
+        });
 
-    it ('correctly orders ranks', () => {
-        const hand = ['Ks', 'As'];
-        expect(represent({hand})).to.equal('AKs');
+        it ('correctly orders ranks', () => {
+            const hand = ['Ks', 'As'];
+            expect(represent({hand})).to.equal('AKs');
+        });
     });
 });
