4ac1262bfd1495e6cd9b8756cebc59c53984306a
Danny Leshem <dleshem@gmail.com>
2019-10-17 18:31:21 +0300

    Added Seats

---
+++ src/Seats.js
@@ -0,0 +1,13 @@
+const Seats = {
+    SB: 'sb',
+    BB: 'bb',
+    UTG: 'utg',
+    UTG1: 'utg+1',
+    UTG2: 'utg+2',
+    LJ: 'lojack',
+    HJ: 'hijack',
+    CO: 'cutoff',
+    BTN: 'button'
+};
+
+export default Seats;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import distance from './distance.js';
 import position from './position.js';
 import Positions from './Positions.js';
 import representRange from './representRange';
+import Seats from './Seats.js';
 
-export {represent, distance, position, Positions, representRange};
+export {represent, distance, position, Positions, representRange, Seats};
