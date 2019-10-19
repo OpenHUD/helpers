0ea56222b13f3612bd41d23a314a579ad1f65e66
Danny Leshem <dleshem@gmail.com>
2019-10-19 17:29:49 +0300

    Fixed typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import represent from './represent.js';
 import distance from './distance.js';
 import position from './position.js';
 import Positions from './Positions.js';
-import representRange from './representRange';
+import representRange from './representRange.js';
 import Seats from './Seats.js';
 import Actions from './Actions.js';
 import extractActions from './extractActions.js';
