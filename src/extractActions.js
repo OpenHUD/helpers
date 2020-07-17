0b5e906feb88e4f0b61c9dc9cfda4f6a15ab731e
Danny Leshem <dleshem@gmail.com>
2020-07-17 16:59:11 +0300

    extractActions now returns MonkerWare branch encoding

--- src/extractActions.js
+++ src/extractActions.js
@@ -1,44 +1,55 @@
 import Actions from './Actions.js';
 
-const getAction = (playerStateBefore, playerStateAfter, effectivePot) => {
+const getAction = (playerStateBefore, playerStateAfter, pot, lastBet) => {
     if (playerStateAfter.isFolded) {
-        return { action: Actions.Fold };
+        return '0'; // fold
     } else {
-        const potDiff = playerStateAfter.pot - playerStateBefore.pot;
-        if (potDiff > 0) {
-            return { action: (playerStateAfter.pot > effectivePot) ? Actions.Bet : Actions.Call, amount: potDiff };
+        if (playerStateAfter.pot <= lastBet) {
+            return '1'; // check or call
+        } else if (playerStateAfter.stack === 0) {
+            return '3'; // raise all-in
+        } else if (playerStateAfter.pot - lastBet === pot + lastBet - playerStateBefore.pot) {
+            return '2'; // raise pot
         } else {
-            return { action: Actions.Check };
+            // TODO: '5' - raise-min
+            const raisePercentage = Math.round(100 * (playerStateAfter.pot - lastBet) / (pot + lastBet));
+            return `4${raisePercentage.toString().padStart(4, '0')}`;
         }
     }
 };
 
-const canAct = playerState => !playerState.isFolded && playerState.stack > 0;
+const canAct = seat => !seat.isFolded && seat.stack > 0;
 
 // Returns a list of actions that turns state1 to state2.
 // Implementation assumes the two states are different, and reachable with at most one action per player.
 //
-// State is an array of PlayerState objects {stack, pot, isFolded, hasAction} with a single hasAction = true.
+// State is an object {pot: Int, seats: Array[PlayerState]}
+// SeatState is an object {stack: Int, pot: Int, isFolded: Boolean, hasAction: Boolean}
+// Exactly one SeatState.hasAction = true
 const extractActions = ({state1, state2}) => {
     const actions = [];
 
-    const pots = state1.map(playerState => playerState.pot);
-    let effectivePot = Math.max(...pots);
+    const pots = state1.seats.map(seat => seat.pot);
+    let totalPot = pots.reduce((total, pot) => total + pot, state1.pot);
+    let maxBet = Math.max(...pots);
+    
 
-    let nextToAct = state1.findIndex(playerState => playerState.hasAction);
+    let nextToAct = state1.seats.findIndex(seat => seat.hasAction);
     do {
-        const playerStateBefore = state1[nextToAct];
-        const playerStateAfter = state2[nextToAct];
+        const seatBefore = state1.seats[nextToAct];
+        const seatAfter = state2.seats[nextToAct];
 
-        if (canAct(playerStateBefore)) {
-            actions.push(getAction(playerStateBefore, playerStateAfter, effectivePot));
-            effectivePot = Math.max(effectivePot, playerStateAfter.pot);
+        if (canAct(seatBefore)) {
+            actions.push(getAction(seatBefore, seatAfter, totalPot, maxBet));
+
+            totalPot += (seatAfter.pot - seatBefore.pot);
+            maxBet = Math.max(maxBet, seatAfter.pot);
         }
 
-        nextToAct = (nextToAct + 1) % state1.length;
-    } while (!state2[nextToAct].hasAction);
+        nextToAct = (nextToAct + 1) % state1.seats.length;
+    } while (!state2.seats[nextToAct].hasAction);
 
-    return actions;
+    return actions.join('.');
 };
 
 
--- test/extractActions.test.js
+++ test/extractActions.test.js
@@ -2,136 +2,191 @@ import extractActions from '../src/extractActions.js';
 import {expect} from 'chai';
 import Actions from '../src/Actions.js';
 
-describe('extractActions', () => {
+describe.only('extractActions', () => {
     describe('actions', () => {
         it ('bet', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 199, pot: 1, hasAction: true },
-                    { stack: 198, pot: 2 }
-                ],
-                state2: [
-                    { stack: 194, pot: 6 },
-                    { stack: 198, pot: 2, hasAction: true }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Bet, amount: 5 }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 195, pot: 5 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+
+                }
+            })).to.equal('40060');
+        });
+
+        it ('pot', () => {
+            expect(extractActions({
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 194, pot: 6 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+
+                }
+            })).to.equal('2');
         });
 
         it ('call', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 199, pot: 1, hasAction: true },
-                    { stack: 198, pot: 2 }
-                ],
-                state2: [
-                    { stack: 198, pot: 2 },
-                    { stack: 198, pot: 2, hasAction: true }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Call, amount: 1 }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 198, pot: 2 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+                }
+            })).to.equal('1');
         });
 
         it ('bet+call', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 200, pot: 0, hasAction: true },
-                    { stack: 199, pot: 1 },
-                    { stack: 198, pot: 2 }
-                ],
-                state2: [
-                    { stack: 195, pot: 5 },
-                    { stack: 195, pot: 5 },
-                    { stack: 198, pot: 2, hasAction: true }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Bet, amount: 5 },
-                { action: Actions.Call, amount: 4 }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 200, pot: 0, hasAction: true },
+                        { stack: 199, pot: 1 },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 195, pot: 5 },
+                        { stack: 195, pot: 5 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+                }
+            })).to.equal('40060.1');
         });
 
         it ('fold', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 200, pot: 0, hasAction: true },
-                    { stack: 199, pot: 1 },
-                    { stack: 198, pot: 2 }
-                ],
-                state2: [
-                    { stack: 200, pot: 0, isFolded: true },
-                    { stack: 199, pot: 1, hasAction: true },
-                    { stack: 198, pot: 2 }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Fold }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 200, pot: 0, hasAction: true },
+                        { stack: 199, pot: 1 },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 200, pot: 0, isFolded: true },
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 198, pot: 2 }
+                    ]
+                }
+            })).to.equal('0');
         });
 
         it ('check', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 199, pot: 1, hasAction: true },
-                    { stack: 199, pot: 1 }
-                ],
-                state2: [
-                    { stack: 199, pot: 1 },
-                    { stack: 199, pot: 1, hasAction: true }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Check }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 199, pot: 1 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1 },
+                        { stack: 199, pot: 1, hasAction: true }
+                    ]
+                }
+            })).to.equal('1');
         });
     });
 
     describe('behavior', () => {
         it ('wraparound', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 199, pot: 1, hasAction: true },
-                    { stack: 198, pot: 2 }
-                ],
-                state2: [
-                    { stack: 194, pot: 6, hasAction: true },
-                    { stack: 180, pot: 20 }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Bet, amount: 5 },
-                { action: Actions.Bet, amount: 18 }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 199, pot: 1, hasAction: true },
+                        { stack: 198, pot: 2 }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 194, pot: 6, hasAction: true },
+                        { stack: 180, pot: 20 }
+                    ]
+                }
+            })).to.equal('2.40100');
         });
 
         it ('skips already-folded players', () => {
-            expect(extractActions({
-                state1: [
-                    { stack: 200, pot: 0, isFolded: true },
-                    { stack: 198, pot: 2 },
-                    { stack: 198, pot: 2, hasAction: true }
-                ],
-                state2: [
-                    { stack: 200, pot: 0, isFolded: true },
-                    { stack: 198, pot: 2, hasAction: true },
-                    { stack: 194, pot: 6  }
-                ]
-            })).to.have.length(1);
+            const branch = extractActions({
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 200, pot: 0, isFolded: true },
+                        { stack: 198, pot: 2 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 200, pot: 0, isFolded: true },
+                        { stack: 198, pot: 2, hasAction: true },
+                        { stack: 194, pot: 6  }
+                    ]
+                }
+            });
+            const actions = branch.split('.');
+            expect(actions).to.have.length(1);
         });
 
         it ('skips already-all-in players', () => {
             expect(extractActions({
-                state1: [
-                    { stack: 0, pot: 2 },
-                    { stack: 198, pot: 2 },
-                    { stack: 198, pot: 2, hasAction: true }
-                ],
-                state2: [
-                    { stack: 0, pot: 2 },
-                    { stack: 198, pot: 2, hasAction: true },
-                    { stack: 192, pot: 8  }
-                ]
-            })).to.deep.equal([
-                { action: Actions.Bet, amount: 6 }
-            ]);
+                state1: {
+                    pot: 0,
+                    seats: [
+                        { stack: 0, pot: 2 },
+                        { stack: 198, pot: 2 },
+                        { stack: 198, pot: 2, hasAction: true }
+                    ]
+                },
+                state2: {
+                    pot: 0,
+                    seats: [
+                        { stack: 0, pot: 2 },
+                        { stack: 198, pot: 2, hasAction: true },
+                        { stack: 192, pot: 8  }
+                    ]
+                }
+            })).to.equal('2');
          });        
     });
 });
