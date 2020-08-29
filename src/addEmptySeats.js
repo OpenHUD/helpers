b7adebcf4e175b722773f186f2d93bf35d98f46d
Danny Leshem <dleshem@gmail.com>
2020-08-29 17:26:47 +0300

    Fixed import

--- src/addEmptySeats.js
+++ src/addEmptySeats.js
@@ -1,18 +1,18 @@
 import cloneDeep from 'lodash/cloneDeep';
-import { centerDistance, getCenter } from './geometry.js';
+import geometry from './geometry.js';
 
 const addEmptySeats = ({seats}) => {
     const newSeats = cloneDeep(seats);
     const maxSeats = newSeats.length <= 6 ? 6 : 9;
     while (newSeats.length < maxSeats) {
         const distances = newSeats.map((s, i, arr) => {
-            return centerDistance(s.box, arr[(i+1) % arr.length].box);
+            return geometry.centerDistance(s.box, arr[(i+1) % arr.length].box);
         });
         const maxIdx = distances.indexOf(Math.max(...distances));
         newSeats.splice(maxIdx == 0 ? 1 : maxIdx, 0, {
             isHero: false,
             name: 'Open Seat',
-            box: [getCenter(geometry.union(newSeats[maxIdx].box, newSeats[(maxIdx+1) % newSeats.length].box))],
+            box: [geometry.getCenter(geometry.union(newSeats[maxIdx].box, newSeats[(maxIdx+1) % newSeats.length].box))],
             isOpen: true,
             sitout: true,
             pot: 0
